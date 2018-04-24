278c457265528856db3369fde0c7fb27bf437468
comp <neethuminit@gmail.com>
2018-04-24 22:33:37 +0530

    First commit

---
+++ src/HousePrice.java
@@ -0,0 +1,17 @@
+
+public class HousePrice {
+	float id,similarity;
+public HousePrice(float id,float similarity) {
+	// TODO Auto-generated constructor stub
+	this.id=id;
+	this.similarity=similarity;
+}
+/* (non-Javadoc)
+ * @see java.lang.Object#toString()
+ */
+@Override
+public String toString() {
+	return "HousePrice [id=" + id + ", similarity=" + similarity + "]";
+}
+
+}
